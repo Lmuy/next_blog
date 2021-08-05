b7dc38f6ea418e5f56952a2bee12afe640ae3405
liukangning <liukangning@kingsoft.com>
2021-08-05 15:15:28 +0800

    增加markdown解析

--- pages/detailed.js
+++ pages/detailed.js
@@ -1,9 +1,54 @@
 import React from 'react';
 import Head from 'next/head'
 import Header from '../components/Header'
-import { Row, Col, Menu } from 'antd';
+import Author from '../components/Author'
+import Advert from '../components/Advert'
+import Footer from '../components/Footer'
+import { Row, Col, Breadcrumb, Affix } from 'antd';
+import { CalendarOutlined, FolderOutlined, FireOutlined } from '@ant-design/icons'
+import '../styles/pages/detailed.css'
+import ReactMarkdown from 'react-markdown';
+import MarkNav from 'markdown-navbar'
+import 'markdown-navbar/dist/navbar.css'
+
 
 const Detailed = () => {
+  const markdown = 
+    '# P01:课程介绍和环境搭建\n' +
+    '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
+    '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
+    '**这是加粗的文字**\n\n' +
+    '*这是倾斜的文字*`\n\n' +
+    '***这是斜体加粗的文字***\n\n' +
+    '~~这是加删除线的文字~~ \n\n'+
+    '\`console.log(111)\` \n\n'+
+    '# p02:来个Hello World 初始Vue3.0\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n'+
+    '***\n\n\n' +
+    '# p03:Vue3.0基础知识讲解\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n'+
+    '# p04:Vue3.0基础知识讲解\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n'+
+    '# p05:Vue3.0基础知识讲解\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n'+
+    '# p06:Vue3.0基础知识讲解\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n'+
+    '# p07:Vue3.0基础知识讲解\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n'+
+    '``` var a=11; ```'
+
   return (
     <div>
       <Head>
@@ -11,13 +56,50 @@ const Detailed = () => {
       </Head>
       <Header/>
       <Row className="comm-main" type="flex" justify="center">
-      <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}>
-          zuo
+        <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}>
+          <div className="bread-div">
+            <Breadcrumb>
+            <Breadcrumb.Item>
+                <a href="/">首页</a>
+              </Breadcrumb.Item>
+              <Breadcrumb.Item>
+                视频列表
+              </Breadcrumb.Item>
+              <Breadcrumb.Item>
+                当前视频
+              </Breadcrumb.Item>
+            </Breadcrumb>
+          </div>
+          <div>
+            <div className="detailed-title">
+              React实战-Blog开发（第10集）
+            </div>
+            <div className="list-icon center">
+              <span><CalendarOutlined />2021-08-05</span>
+              <span><FolderOutlined />视频教程</span>
+              <span><FireOutlined />4567人</span>
+            </div>
+            <div className="detailed-content">
+              <ReactMarkdown>{markdown}</ReactMarkdown>
+            </div>
+          </div>
         </Col>
         <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
-          you
+          <Author />
+          <Advert />
+          <Affix offsetTop={5}>
+            <div className="detailed-nav comm-box">
+              <div className="nav-title">文章标题</div>
+              <MarkNav
+                className="article-menu"
+                source={markdown}
+                ordered={false}
+              />
+            </div>
+          </Affix>
         </Col>
       </Row>
+      <Footer />
     </div>
   )
 }
