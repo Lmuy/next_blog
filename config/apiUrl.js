3a0286937ffa9d48bf1202c595381d878c1f4257
liukangning <liukangning@kingsoft.com>
2021-08-10 15:39:08 +0800

    不同类别文章列表页

--- config/apiUrl.js
+++ config/apiUrl.js
@@ -3,7 +3,8 @@ let ipUrl = 'http://127.0.0.1:7001/default/'
 let servicePath = {
   getArticleList: `${ipUrl}getArticleList`,
   getArticleById: `${ipUrl}getArticleById/`,
-  getTypeInfo: `${ipUrl}getTypeInfo`
+  getTypeInfo: `${ipUrl}getTypeInfo`,
+  getListById: `${ipUrl}getListById/`,
 }
 
 export default servicePath
